e6c89a3fb255b55ef83343deb4f802acf3830713
BasantSingh2022 <basantkumar2022@gmail.com>
2026-01-19 13:48:01 +0530

    ci-cd pipeline

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get('/health', (req, res) => {
 });
 
 // Error handling
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
